fafc4635e953074a2d9ff8f2fa652bdf6184dbc9
Jai Bagaria <deviljai1999@gmail.com>
2023-11-09 00:48:21 +0530

    hy

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -2,24 +2,27 @@ import React from 'react'
 import Contact from '../Contact/Contact'
 import './Background.css'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Home from '../Home/Home';
-import Sidebar from '../Sidebar/Sidebar';
-import Portfolio from '../Portfolio/Portfolio';
+import Services from '../Services/Services';
+import Navbar from '../Navbar/Navbar';
+import Sidebar from '../Sidebar/Sidebar'
+import Home from '../Home/Home'
+// import Popup from '../Popup';
+import Module from '../Popup';
+
 
 function Background() {
   return (
     <>
-    <div className="background" style={{backgroundColor:"#121212" ,width:"100%",height:"100%"}}>
+    <div className="background" style={{backgroundColor:"#121212" ,width:"100%",height:"100vh"}}>
         <div className="container outer_container pt-5 pb-1 " >
         <Router>
-          {/* <Sidebar/> */}
+     <Sidebar/>
         <Routes>
 
-          <Route path="/" element={<Portfolio/>}></Route>
-          {/* <Route path="/" element={<Home/>}></Route> */}
-          {/* <Route path="/about" element={<About/>}></Route>
-          <Route path="/services" element={<Services/>}></Route>
-          <Route path="/contact" element={<Contact  />}></Route>
+          <Route path="/" element={<Contact/>}></Route>
+           <Route path="/popup" element={<Module/>}></Route>
+          <Route path="/home" element={<Home/>}></Route>
+        {/*  <Route path="/contact" element={<Contact  />}></Route>
           <Route path="/work" element={<Project  />}></Route>
           <Route path="/popup" element={<Modal  />}></Route> */}
 
